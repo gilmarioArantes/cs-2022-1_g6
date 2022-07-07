38a3c4671bbec11d01e2853581721f86f029b82a
igorpadua <igorpadua@tutanota.com>
2022-07-07 10:02:18 -0300

    Receitas está relacionado com usuários agora

--- api-receitas/src/app.module.ts
+++ api-receitas/src/app.module.ts
@@ -3,10 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { RecibesModule } from './recibes/recibes.module';
+import { RecipesModule } from './recipes/recipes.module';
 
 @Module({
-  imports: [AuthModule, UserModule, RecibesModule],
+  imports: [AuthModule, UserModule, RecipesModule],
   controllers: [AppController],
   providers: [AppService],
 })
